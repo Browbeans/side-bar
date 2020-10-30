39614c62fe0f00b117eb73dd8c1dce540634cb18
Browbeans <oliverjohansson3@gmail.com>
2020-10-30 15:11:18 +0100

    Added some repsonsive things

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const side = document.getElementById("sideBar")
-let text = document.querySelector("p")
+const h2 = document.querySelector ("h2")
+
 
 document.getElementById("rightclick").onclick = function () {
     side.style.marginLeft = "0%"
@@ -10,8 +11,23 @@ document.getElementById("toggleAway").onclick = function (){
     side.style.transition ="all 500ms"
 }
 
-document.getElementsByClassName("search").onmouseover = function() {
-    text.style.display = "block"
-    console.log("da")
+window.onresize = function() {
+    if(window.innerWidth > 1200){
+        side.style.width = "4rem" 
+        side.style.marginLeft = "0"
+    }
+    else if (window.innerWidth < 1200) { 
+        side.style.width = "15rem"
+        side.style.marginLeft = "0"
+    }
 }
+
+if (side.style.width == "6rem") {
+    h2.innerText = ""
+}
+
+side.onmouseover = function() {
+    side.style.width = "15rem"
+}
+
 

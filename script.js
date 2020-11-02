4be51108e2885dc2c2d2326a4f726ce646b583db
Browbeans <oliverjohansson3@gmail.com>
2020-11-02 16:18:38 +0100

    add content, script and styiling for the slidshow

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+
+
 const side = document.getElementById("sideBar")
 const h2 = document.querySelector ("h2")
+
+const image = document.querySelector("img")
+const image1 = document.getElementById("firstImage")
+const image2 = document.getElementById("secondImage")
+const image3 = document.getElementById("thirdImage")
+
+
+let imageArray = [image1, image2, image3]
+
+
+setInterval(startSlideShow, 3000)
+function startSlideShow(){
+    for(i = 0; i < imageArray.length; i++){
+        if(imageArray[i].classList.contains("show")){
+            imageArray[i].classList.toggle("show")
+            imageArray[2].classList.toggle("show")
+        }
+        else if(imageArray[2].classList.contains("show")){
+            imageArray[i].classList.toggle("show")
+            imageArray[2].classList.toggle("show")
+        }
+    }
+}
 
 
 document.getElementById("rightclick").onclick = function () {
@@ -22,12 +47,12 @@ window.onresize = function() {
     }
 }
 
-if (side.style.width == "6rem") {
-    h2.innerText = ""
-}
-
 side.onmouseover = function() {
     side.style.width = "15rem"
 }
 
-
+side.onmouseout = function() {
+    if(window.innerWidth > 1200){
+    side.style.width = "4rem"
+    } 
+}
